089da02b8d742ca472baa5eabebd19d06a259cf0
schwehr <schwehr@a19cddd1-5311-0410-bb07-9ca93daf0f0b>
2010-07-07 22:53:29 +0000

    compiles but does not work
    git-svn-id: https://cowfish.unh.edu/projects/schwehr/trunk/src/libais@14085 a19cddd1-5311-0410-bb07-9ca93daf0f0b

--- ais15.cpp
+++ ais15.cpp
@@ -2,14 +2,15 @@
 
 #include "ais.h"
 
-Ais9::Ais9(const char *nmea_payload) {
+Ais15::Ais15(const char *nmea_payload) {
     assert(nmea_payload);
     init();
 
-    const int num_char = str_len(nmea_payload);
-    if !(num_char == 162/6 || num_char== ) {
-      status = AIS_ERR_BAD_BIT_COUNT;
-      return;
+    const int num_char = std::strlen(nmea_payload);
+    if (!(num_char >= 14/6 && num_char<=27)) {
+        // 88-160 bits
+        status = AIS_ERR_BAD_BIT_COUNT;
+        return;
     }
 
     std::bitset<162> bs; // 160 / 6 = 26.66
@@ -17,6 +18,9 @@ Ais9::Ais9(const char *nmea_payload) {
     if (had_error()) return;
 
     message_id = ubits(bs, 0, 6);
-    if (9 != message_id) { status = AIS_ERR_WRONG_MSG_TYPE; return; }
+    if (15 != message_id) { status = AIS_ERR_WRONG_MSG_TYPE; return; }
     repeat_indicator = ubits(bs,6,2);
     mmsi = ubits(bs,8,30);
+
+    // FIX: write the rest
+}
