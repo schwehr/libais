0b77a38052644df1e6fa8a28e78e70abcf2a0f14
Kurt Schwehr <schwehr@google.com>
2012-10-26 10:52:05 -0700

    test all top level msg types except ais20

---
+++ test/test_decode.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""Test the top level decoders with a single message each.
+
+"""
+
+import ais
+import unittest
+import test_data
+
+class AisTopLevelDecoders(unittest.TestCase):
+
+  def setUp(self):
+    self.maxDiff = None
+
+  def testAll(self):
+    # TODO: message 20
+    for entry in test_data.top_level:
+      body = ''.join([line.split(',')[5] for line in entry['nmea']])
+      pad = int(entry['nmea'][-1].split('*')[0][-1])
+      msg = ais.decode(body, pad)
+      #print msg['id'], body, pad
+      self.assertDictEqual(msg, entry['result'])
+
+
+if __name__=='__main__':
+  unittest.main()
