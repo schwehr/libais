9dca22f404cc1afef1121439887f3e6c3ded405c
Kurt Schwehr <schwehr@google.com>
2012-10-31 15:44:53 -0700

    formatting

--- ais10.cpp
+++ ais10.cpp
@@ -4,16 +4,23 @@
 
 Ais10::Ais10(const char *nmea_payload, const size_t pad) {
     assert(nmea_payload);
+    assert(pad < 6);
     init();
 
-    if (pad != 0 || strlen(nmea_payload) != 72/6) { status = AIS_ERR_BAD_BIT_COUNT; return; }
+    if (pad != 0 || strlen(nmea_payload) != 12) {
+      status = AIS_ERR_BAD_BIT_COUNT;
+      return;
+    }
 
     bitset<72> bs;
     status = aivdm_to_bits(bs, nmea_payload);
     if (had_error()) return;
 
     message_id = ubits(bs, 0, 6);
-    if (10 != message_id) { status = AIS_ERR_WRONG_MSG_TYPE; return; }
+    if (message_id != 10) {
+      status = AIS_ERR_WRONG_MSG_TYPE;
+      return;
+    }
     repeat_indicator = ubits(bs, 6, 2);
     mmsi = ubits(bs, 8, 30);
 
