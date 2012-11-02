f5b0ef1fd40e3695da62471aefa27e80d4141237
Kurt Schwehr <schwehr@google.com>
2012-11-02 12:36:17 -0700

    formatting

--- ais8_001_26.cpp
+++ ais8_001_26.cpp
@@ -1,10 +1,10 @@
-// The USCG RTCM SC 121 Environmental Sensor Message tested in the Tampa, FL from 200[78] util ?
+// The USCG RTCM SC 121 Environmental Sensor Message tested in
+// Tampa, FL from 200[78] util ?
 
 // http://www.dtic.mil/cgi-bin/GetTRDoc?AD=ADA504755
 // Phase I Summary Report on AIS Transmit Project (Environmental Message)
 
 #include "ais.h"
-
 
 Ais8_1_26_Location::Ais8_1_26_Location(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   x = sbits(bs, offset, 28) / 600000.;
@@ -15,12 +15,10 @@ Ais8_1_26_Location::Ais8_1_26_Location(const bitset<AIS8_MAX_BITS> &bs, const si
   spare = ubits(bs, offset+73, 12);
 }
 
-
 Ais8_1_26_Station::Ais8_1_26_Station(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   name = ais_str(bs, offset, 84);
   spare = ubits(bs, offset+84, 1);
 }
-
 
 Ais8_1_26_Wind::Ais8_1_26_Wind(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   wind_speed = ubits(bs, offset, 7);
@@ -38,7 +36,6 @@ Ais8_1_26_Wind::Ais8_1_26_Wind(const bitset<AIS8_MAX_BITS> &bs, const size_t off
   spare = ubits(bs, offset+82, 3);
 }
 
-
 Ais8_1_26_WaterLevel::Ais8_1_26_WaterLevel(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   type = bs[offset];
   level = sbits(bs, offset+1, 16) / 100.;
@@ -54,7 +51,6 @@ Ais8_1_26_WaterLevel::Ais8_1_26_WaterLevel(const bitset<AIS8_MAX_BITS> &bs, cons
   spare = ubits(bs, offset+68, 17);
 }
 
-
 Ais8_1_26_Curr2D::Ais8_1_26_Curr2D(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   for (size_t idx = 0; idx < 3; idx++) {
     size_t start = offset + idx * 26;
@@ -65,7 +61,6 @@ Ais8_1_26_Curr2D::Ais8_1_26_Curr2D(const bitset<AIS8_MAX_BITS> &bs, const size_t
   type = ubits(bs, offset+78, 3);
   spare = ubits(bs, offset+81, 4);
 }
-
 
 Ais8_1_26_Curr3D::Ais8_1_26_Curr3D(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   for (size_t idx = 0; idx < 2; idx++) {
@@ -79,7 +74,6 @@ Ais8_1_26_Curr3D::Ais8_1_26_Curr3D(const bitset<AIS8_MAX_BITS> &bs, const size_t
   spare = ubits(bs, offset+69, 16);
 }
 
-
 Ais8_1_26_HorzFlow::Ais8_1_26_HorzFlow(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   for (size_t idx = 0; idx < 2; idx++) {
     size_t start = offset + idx * 42;
@@ -91,7 +85,6 @@ Ais8_1_26_HorzFlow::Ais8_1_26_HorzFlow(const bitset<AIS8_MAX_BITS> &bs, const si
   }
   spare = bs[offset+84];
 }
-
 
 Ais8_1_26_SeaState::Ais8_1_26_SeaState(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   swell_height = ubits(bs, offset, 8) / 10.;
@@ -109,7 +102,6 @@ Ais8_1_26_SeaState::Ais8_1_26_SeaState(const bitset<AIS8_MAX_BITS> &bs, const si
   salinity = ubits(bs, offset+76, 9) / 10.;
 }
 
-
 Ais8_1_26_Salinity::Ais8_1_26_Salinity(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   water_temp = ubits(bs, offset, 10) / 10. - 10;
   conductivity = ubits(bs, offset+10, 10) / 100.;
@@ -120,7 +112,6 @@ Ais8_1_26_Salinity::Ais8_1_26_Salinity(const bitset<AIS8_MAX_BITS> &bs, const si
   spare[0] = ubits(bs, offset+50, 32);
   spare[1] = ubits(bs, offset+82, 3);
 }
-
 
 Ais8_1_26_Wx::Ais8_1_26_Wx(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   air_temp = sbits(bs, offset, 11) / 10.;
@@ -136,7 +127,6 @@ Ais8_1_26_Wx::Ais8_1_26_Wx(const bitset<AIS8_MAX_BITS> &bs, const size_t offset)
   spare = ubits(bs, offset+60, 25);
 }
 
-
 Ais8_1_26_AirDraught::Ais8_1_26_AirDraught(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   draught = ubits(bs, offset, 13) / 100.;
   gap = ubits(bs, offset+13, 13) / 10.;
@@ -148,25 +138,48 @@ Ais8_1_26_AirDraught::Ais8_1_26_AirDraught(const bitset<AIS8_MAX_BITS> &bs, cons
   spare = ubits(bs, offset+57, 28);
 }
 
-
 Ais8_1_26_SensorReport* ais8_1_26_sensor_report_factory(const bitset<AIS8_MAX_BITS> &bs, const size_t offset) {
   const Ais8_1_26_SensorEnum rpt_type = (Ais8_1_26_SensorEnum)ubits(bs, offset, 4);
+
+  // WARNING: out of order decoding
+  // Only get the report header if we can decode the type
   const size_t rpt_start = offset+27;  // skip tp after site_id
 
-  Ais8_1_26_SensorReport *rpt = 0;
-  // WARNING: out of order decoding.  Only get the report header if we can decode the type.
+  Ais8_1_26_SensorReport *rpt = NULL;
   switch (rpt_type) {
-  case AIS8_1_26_SENSOR_LOCATION:    rpt = new Ais8_1_26_Location(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_STATION:     rpt = new Ais8_1_26_Station(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_WIND:        rpt = new Ais8_1_26_Wind(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_WATER_LEVEL: rpt = new Ais8_1_26_WaterLevel(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_CURR_2D:     rpt = new Ais8_1_26_Curr2D(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_CURR_3D:     rpt = new Ais8_1_26_Curr3D(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_HORZ_FLOW:   rpt = new Ais8_1_26_HorzFlow(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_SEA_STATE:   rpt = new Ais8_1_26_SeaState(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_SALINITY:    rpt = new Ais8_1_26_Salinity(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_WX:          rpt = new Ais8_1_26_Wx(bs, rpt_start); break;
-  case AIS8_1_26_SENSOR_AIR_DRAUGHT: rpt = new Ais8_1_26_AirDraught(bs, rpt_start); break;
+  case AIS8_1_26_SENSOR_LOCATION:
+    rpt = new Ais8_1_26_Location(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_STATION:
+    rpt = new Ais8_1_26_Station(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_WIND:
+    rpt = new Ais8_1_26_Wind(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_WATER_LEVEL:
+    rpt = new Ais8_1_26_WaterLevel(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_CURR_2D:
+    rpt = new Ais8_1_26_Curr2D(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_CURR_3D:
+    rpt = new Ais8_1_26_Curr3D(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_HORZ_FLOW:
+    rpt = new Ais8_1_26_HorzFlow(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_SEA_STATE:
+    rpt = new Ais8_1_26_SeaState(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_SALINITY:
+    rpt = new Ais8_1_26_Salinity(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_WX:
+    rpt = new Ais8_1_26_Wx(bs, rpt_start);
+    break;
+  case AIS8_1_26_SENSOR_AIR_DRAUGHT:
+    rpt = new Ais8_1_26_AirDraught(bs, rpt_start);
+    break;
   case AIS8_1_26_SENSOR_RESERVED_11: break;  // Leave rpt == 0 to indicate error
   case AIS8_1_26_SENSOR_RESERVED_12: break;
   case AIS8_1_26_SENSOR_RESERVED_13: break;
@@ -175,17 +188,17 @@ Ais8_1_26_SensorReport* ais8_1_26_sensor_report_factory(const bitset<AIS8_MAX_BI
   default:
     {}  // Leave rpt == 0 to indicate error
   }
-  if (rpt) {
-    rpt->report_type = rpt_type;
-    rpt->utc_day = ubits(bs, offset+4, 5);
-    rpt->utc_hr = ubits(bs, offset+9, 5);
-    rpt->utc_min = ubits(bs, offset+14, 6);
-    rpt->site_id = ubits(bs, offset+20, 7);
-  }
 
+  if (!rpt)
+    return rpt;
+
+  rpt->report_type = rpt_type;
+  rpt->utc_day = ubits(bs, offset+4, 5);
+  rpt->utc_hr = ubits(bs, offset+9, 5);
+  rpt->utc_min = ubits(bs, offset+14, 6);
+  rpt->site_id = ubits(bs, offset+20, 7);
   return rpt;
 }
-
 
 Ais8_1_26::Ais8_1_26(const char *nmea_payload, const size_t pad) {
   assert(nmea_payload);
@@ -194,7 +207,10 @@ Ais8_1_26::Ais8_1_26(const char *nmea_payload, const size_t pad) {
   init();
 
   const int num_bits = strlen(nmea_payload) * 6 - pad;
-  if (168 > num_bits || num_bits > 1098) { status = AIS_ERR_BAD_BIT_COUNT; return; }
+  if (168 > num_bits || num_bits > 1098) {
+    status = AIS_ERR_BAD_BIT_COUNT;
+    return;
+  }
 
   bitset<MAX_BITS> bs;
   status = aivdm_to_bits(bs, nmea_payload);
@@ -218,7 +234,6 @@ Ais8_1_26::Ais8_1_26(const char *nmea_payload, const size_t pad) {
     }
   }
 }
-
 
 Ais8_1_26::~Ais8_1_26() {
   for (size_t i = 0; i < reports.size(); i++) {
