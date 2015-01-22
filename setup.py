9307d88910181bbe8aa786faff0dd3aa13ff9fe5
Kurt Schwehr <schwehr@google.com>
2015-01-22 11:39:52 -0800

    Add -std=c++11 for Linux and Mac python module building.  Lint setup.py

--- setup.py
+++ setup.py
@@ -1,59 +1,68 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import setup, find_packages, Extension
 
-version = open('VERSION').readline().strip()
+VERSION = open('VERSION').readline().strip()
 
-ais_module = Extension('_ais',
-                    sources = ['ais_py.cpp',
-                               'ais.cpp',
-                               'ais1_2_3.cpp', # Class A position
-                               'ais4_11.cpp', # Basestation report, '=' - UTC time response
-                               'ais5.cpp', # Static data report / ship name
-                               'ais6.cpp', # Addressed binary message (ABM)
-                               'ais7_13.cpp',
-                               'ais8.cpp', # Broadcast binary message (BBM)
-                               'ais8_001_22.cpp', # Area notice
-                               'ais8_001_26.cpp', # Environmental Sensor Report
-                               'ais9.cpp',
-                               'ais10.cpp', # :
-                               # 11 See 4 - ;
-                               'ais12.cpp', # <
-                               # 13 See 7 - =
-                               'ais14.cpp', # >
-                               'ais15.cpp', # ?
-                               'ais16.cpp', # @
-                               'ais17.cpp', # A
-                               'ais18.cpp', # B
-                               'ais19.cpp', # C
-                               'ais20.cpp', # D
-                               'ais21.cpp', # E
-                               'ais22.cpp', # F
-                               'ais23.cpp', # G
-                               'ais24.cpp', # H
-                               'ais25.cpp', # I - single slot binary message
-                               'ais26.cpp', # J - Multi-slot binary message with comm-state
-                               'ais27.cpp', # K - Long-range position
-                               # 'ais28.cpp', # L - Not yet defined
-                               ])
+EXTRA_COMPILE_ARGS = []
+if sys.platform in ('linux2', 'darwin'):
+    EXTRA_COMPILE_ARGS = ['-std=c++11']
 
-setup (name = 'libais',
-       version = version,
-       description = 'Automatic Identification System decoding - ship tracking',
-       author='Kurt Schwehr',
-       author_email='schwehr@gmail.com',
-       url='https://github.com/schwehr/libais',
-       license='Apache 2.0',
-       ext_modules = [ais_module],
-       classifiers=[
-            'License :: OSI Approved :: Apache Software License',
-            'Topic :: System :: Networking',
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Science/Research',
-            'Topic :: Communications',
-            'Topic :: Scientific/Engineering :: Information Analysis',
-            'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
-            'Topic :: Scientific/Engineering :: GIS',
-    ],
-       scripts = ["bin/aisdecode"]
-       )
+AIS_MODULE = Extension(
+    '_ais',
+    extra_compile_args=EXTRA_COMPILE_ARGS,
+    sources=[
+        'ais_py.cpp',
+        'ais.cpp',
+        'ais1_2_3.cpp', # Class A position
+        'ais4_11.cpp', # Basestation report, '=' - UTC time response
+        'ais5.cpp', # Static data report / ship name
+        'ais6.cpp', # Addressed binary message (ABM)
+        'ais7_13.cpp',
+        'ais8.cpp', # Broadcast binary message (BBM)
+        'ais8_001_22.cpp', # Area notice
+        'ais8_001_26.cpp', # Environmental Sensor Report
+        'ais9.cpp',
+        'ais10.cpp', # :
+        # 11 See 4 - ;
+        'ais12.cpp', # <
+        # 13 See 7 - =
+        'ais14.cpp', # >
+        'ais15.cpp', # ?
+        'ais16.cpp', # @
+        'ais17.cpp', # A
+        'ais18.cpp', # B
+        'ais19.cpp', # C
+        'ais20.cpp', # D
+        'ais21.cpp', # E
+        'ais22.cpp', # F
+        'ais23.cpp', # G
+        'ais24.cpp', # H
+        'ais25.cpp', # I - single slot binary message
+        'ais26.cpp', # J - Multi-slot binary message with comm-state
+        'ais27.cpp', # K - Long-range position
+        # 'ais28.cpp', # L - Not yet defined
+        ])
+
+setup(name='libais',
+      version=VERSION,
+      description='Automatic Identification System decoding - ship tracking',
+      author='Kurt Schwehr',
+      author_email='schwehr@gmail.com',
+      url='https://github.com/schwehr/libais',
+      license='Apache 2.0',
+      ext_modules=[AIS_MODULE],
+      classifiers=[
+           'License :: OSI Approved :: Apache Software License',
+           'Topic :: System :: Networking',
+           'Development Status :: 4 - Beta',
+           'Intended Audience :: Science/Research',
+           'Topic :: Communications',
+           'Topic :: Scientific/Engineering :: Information Analysis',
+           'Topic :: Scientific/Engineering :: '
+           'Interface Engine/Protocol Translator',
+           'Topic :: Scientific/Engineering :: GIS',
+           ],
+      scripts=["bin/aisdecode"])
