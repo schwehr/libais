ab855dac5cef28eb7d9cf0d8a944fd479940dd32
Kevin Wurster <kevin@skytruth.org>
2015-03-16 12:02:03 -0400

    MVP Benthos validator.

--- utils/benthos_validate.py
+++ utils/benthos_validate.py
@@ -2,7 +2,7 @@
 
 
 """
-Validator to ensure libais output is what Benthos expects to receive
+Validator to ensure libais can produce Benthos's expected input
 """
 
 
@@ -12,20 +12,39 @@ import unittest
 import sys
 
 
-# Keys are fieldnames and values are dictionaries containing 'test', a function verifying if a value is acceptable,
-# 'good' a value that will pass the test, and 'bad' a value that will not pass the test.
-# The unittest for this utility check to see if good and bad values are iterable so testing multiple values is possible.
+# In order to ease test maintenance as outputs and inputs change the data structure below contains a test for every
+# field, a value that will pass the test, and a value that will fail the test.  All information is pulled from GPSD
+# (http://catb.org/gpsd/AIVDM.html) and assumes two things:
+#   1. Fieldnames are unique across all messages
+#   2. Fields appearing in different message types contain the same information.  For instance, the field 'sog'
+#      appears in multiple types but is always speed over ground in the same units in every message.
+#
+# Keys are fieldnames and values are dictionaries containing three keys:
+#   1. test - a function that verifies a value is acceptable for this field
+#   2. good - a value that will pass the test function
+#   3. bad  - a value that will not pass the test function
+#
+# Some fields are flags with values 0 or 1 that may be switched to Python's bool in the future but in the meantime
+# we want to be positive these values are int's.  Since bool subclasses int expressions like `0 in (1, 0)` and
+# `True in (0, 1)` both evaluate as `True` which could yield unexpected results.  Any test that expects an int
+# also checks to make sure that int is NOT a bool, even if the field is a range and will never be bool.  Better to be
+# safe here than be forced to debug some potentially ambiguous bugs elsewhere.
 MSG_VALIDATION_LOOKUP = {
+
+    # Comments about where fields are pulled from just reference where the description was found in the GPSD
+    # specification http://catb.org/gpsd/AIVDM.html
+    # They're not critical but if there is a discrepancy between what is expected and how the test is written
+    # it may help us figure out why.
 
     # Pulled from types 1, 2, and 3 GPSD spec
     'type': {
-        'test': lambda x: x in range(1, 28),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(1, 28),
         'good': 5,
         'bad': -1
     },
     'repeat': {
-        'test': lambda x: 0 <= x <= 2 ** 2,
-        'good': 10,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 <= x <= 2 ** 2,
+        'good': 4,
         'bad': -1
     },
     'mmsi': {
@@ -34,74 +53,74 @@ MSG_VALIDATION_LOOKUP = {
         'bad': 1234
     },
     'status': {
-        'test': lambda x: x in range(0, 16),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 16),
         'good': 2,
         'bad': -10
     },
     'turn': {
-        'test': lambda x: x in range(-127, 129),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(-127, 129),
         'good': 125,
         'bad': -1000
     },
     'sog': {
-        'test': lambda x: 0 <= x <= 102.2 or x is 1022,
+        'test': lambda x: isinstance(x, float) and 0 <= x <= 102.2 or x == 1022,
         'good': 1022,
         'bad': 103
     },
     'accuracy': {
-        'test': lambda x: x in (0, 1),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),
         'good': 0,
         'bad': 2,
     },
     'lat': {
-        'test': lambda x: -90 <= x <= 90 or x is 91,
+        'test': lambda x: isinstance(x, float) and -90 <= x <= 90 or x == 91,
         'good': 91,
         'bad': -100
     },
     'lon': {
-        'test': lambda x: -180 <= x <= 180 or x is 181,  # TODO: Should -180 be a valid value?  Maybe `-180 < x` instead?
+        'test': lambda x: isinstance(x, float) and -180 <= x <= 180 or x == 181,  # TODO: Should -180 be a valid value?  Maybe `-180 < x` instead?
         'good': 181,
         'bad': -180.1
     },
     'course': {
-        'test': lambda x: 0 <= x <= 359 or x is 3600,  # TODO: Should -90 be a valid value?  Maybe `-90 < x` instead?
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 <= x <= 359 or x == 3600,  # TODO: Should -90 be a valid value?  Maybe `-90 < x` instead?
         'good': 3600,
         'bad': 360
     },
     'heading': {
-        'test': lambda x: 0 <= x <= 359 or x is 511,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 <= x <= 359 or x == 511,
         'good': 511,
         'bad': -102
     },
     'second': {
-        'test': lambda x: x in range(0, 64),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 64),
         'good': 63,
         'bad': 64
     },
     'maneuver': {
-        'test': lambda x: x in (1, 2),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (1, 2),
         'good': 2,
         'bad': 3
     },
     'raim': {
-        'test': lambda x: x in (0, 1),  # TODO: bool is more Pythonic if the field is actually boolean and not state
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # TODO: bool is more Pythonic if the field is actually boolean and not state
         'good': 0,
         'bad': -2
     },
     'radio': {
-        'test': lambda x: x is not False,  # TODO: What will this value be?
-        'good': True,
+        'test': lambda x: x is None,  # TODO: What will this value be?
+        'good': None,
         'bad': False
     },
 
     # Pulled from type 5 GPSD spec
     'ais_version': {
-        'test': lambda x: x in (0, 1, 2, 3),  # TODO: Should always be 0 right now.  The other vals are reserved.
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1, 2, 3),  # TODO: Should always be 0 right now.  The other vals are reserved.
         'good': 2,
         'bad': True
     },
     'imo': {
-        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 30,  # TODO: Length=30 bits
+        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 30,
         'good': 'value',
         'bad': True,
     },
@@ -111,72 +130,72 @@ MSG_VALIDATION_LOOKUP = {
         'bad': json
     },
     'shipname': {
-        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 120,  # TODO: Length=120 bits
+        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 120,
         'good': 'good value',
         'bad': False
     },
     'shiptype': {
-        'test': lambda x: x in range(0, 100),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 100),
         'good': 87,
         'bad': str
     },
     'to_bow': {
-        'test': lambda x: 0 <= x <= 2 ** 9,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 <= x <= 2 ** 9,
         'good': 1,
         'bad': -1
     },
     'to_stern': {
-        'test': lambda x: 0 <= x <= 2 ** 9,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 <= x <= 2 ** 9,
         'good': 0,
         'bad': tuple
     },
     'to_port': {
-        'test': lambda x: 0 <= x <= 2 ** 6,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 <= x <= 2 ** 6,
         'good': 1,
         'bad': -34
     },
     'to_starboard': {
-        'test': lambda x: 0 <= x <= 2 ** 6,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 <= x <= 2 ** 6,
         'good': 0,
         'bad': False,
     },
     'epfd': {
-        'test': lambda x: x in range(0, 9),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 9),
         'good': 8,
         'bad': 10
     },
     'month': {
-        'test': lambda x: x in range(0, 13),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 13),
         'good': 12,
         'bad': 'asdf'
     },
     'day': {
-        'test': lambda x: x in range(0, 32),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 32),
         'good': 1,
         'bad': ':'
     },
     'hour': {
-        'test': lambda x: x in range(0, 25),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 25),
         'good': 23,
         'bad': ','
     },
     'minute': {
-        'test': lambda x: x in range(0, 61),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in range(0, 61),
         'good': 54,
         'bad': '*'
     },
     'draught': {
-        'test': lambda x: 0 < x <= 2 ** 8,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and 0 < x <= 2 ** 8,
         'good': 1,
         'bad': 2 ** 8 + 1
     },
     'destination': {
-        'test': lambda x: isinstance(x, str) and x <= 2 ** 120,
+        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 120,
         'good': 'v',
         'bad': 2 * 2
     },
     'dte': {
-        'test': lambda x: x in (0, 1),  # TODO: Switch to a more Pythonic bool if this is actually bolean and not a status
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # TODO: Switch to a more Pythonic bool if this is actually bolean and not a status
         'good': 0,
         'bad': 8
     },
@@ -193,37 +212,37 @@ MSG_VALIDATION_LOOKUP = {
         'bad': -1
     },
     'speed': {
-        'test': lambda x: x <= 2 ** 10,
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x <= 2 ** 10,
         'good': 0,
         'bad': str
     },
     'cs': {
-        'test': lambda x: x in (0, 1),  # Not bool - state
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # Not bool - state
         'good': 0,
         'bad': 7
     },
     'display': {
-        'test': lambda x: x in (0, 1),  # Not bool - state
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # Not bool - state
         'good': 1,
         'bad': 'j'
     },
     'dsc': {
-        'test': lambda x: x in (0, 1),  # TODO: Switch to a more Pythonic bool?
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # TODO: Switch to a more Pythonic bool?
         'good': 1,
         'bad': -45
     },
     'band': {
-        'test': lambda x: x in (0, 1),  # TODO: Switch to a more Pythonic bool?
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # TODO: Switch to a more Pythonic bool?
         'good': 0,
         'bad': 4
     },
     'msg22': {
-        'test': lambda x: x in (0, 1),  # TODO: Switch to a more Pythonic bool?
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # TODO: Switch to a more Pythonic bool?
         'good': 0,
         'bad': -2
     },
     'assigned': {
-        'test': lambda x: x in (0, 1),  # TODO: Switch to a more Pythonic bool?
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # TODO: Switch to a more Pythonic bool?
         'good': 1,
         'bad': -33
     },
@@ -232,33 +251,34 @@ MSG_VALIDATION_LOOKUP = {
 
     # Pulled from type 24 GPSD spec
     'partno': {
-        'test': lambda x: x in (0, 1),
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),
         'good': 0,
         'bad': -1
     },
     'vendorid': {
-        'test': lambda x: isinstance(x, str) and x <= 2 ** 18,
+        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 18,
         'good': 'this is a gooooooooood value',
         'bad': int
     },
     'model': {
-        'test': lambda x: isinstance(x, str) and x <= 2 ** 4,
+        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 4,
         'good': 'something',
         'bad': 333
     },
     'serial': {
-        'test': lambda x: isinstance(x, str) and x <= 2 ** 20,
+        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 20,
         'good': 'pawoeiras',
         'bad': -1
     },
     'mothership_mmsi': {
-        'test': lambda x: isinstance(x, str) and x <= 2 ** 30,
+        'test': lambda x: isinstance(x, str) and len(x) <= 2 ** 30,
         'good': 'done ... finally ...',
         'bad': -200
     },
+
     # Pulled from type 27 GPSD spec
     'gnss': {
-        'test': lambda x: x in (0, 1),  # Not bool - state
+        'test': lambda x: isinstance(x, int) and not isinstance(x, bool) and x in (0, 1),  # Not bool - state
         'good': 0,
         'bad': 3
     }
@@ -296,7 +316,9 @@ MSG_TYPE_FIELDS = {
         'type', 'repeat', 'mmsi', 'partno', 'shipname', 'shiptype', 'vendorid', 'model', 'serial', 'callsign',
         'to_bow', 'to_stern', 'to_port', 'to_starboard', 'mothership_mmsi'
     ],
-    27: None
+    27: [
+        'type', 'repeat', 'mmsi', 'accuracy', 'raim', 'status', 'lon', 'lat', 'speed', 'course', 'gnss'
+    ]
 }
 
 
@@ -308,16 +330,19 @@ def validate_messages(messages, err=None):
     Example:
 
         >>> import json
-        >>> with open('Messages.json') as i_f:
-        ...     with open('Logfile') as l_f:
-        ...         print(validate_messages((json.loads(msg) for msg in i_f), err=l_f))
+        >>> with open('Messages.json') as infile:
+        ...     with open('Logfile') as logfile:
+        ...         print(validate_messages(
+        ...             (json.loads(msg) for msg in infile), err=logfile))
 
     Parameters
     ----------
     msg : iter
         An iterable producing one AIS message as a dictionary every iteration.
     err : file, optional
-        File-like object where failed messages are written.
+        File-like object where errors are logged and failed messages are written.
+        A message with multiple invalid fields will have multiple errors in this
+        file.
 
     Returns
     -------
@@ -328,12 +353,21 @@ def validate_messages(messages, err=None):
     return_val = True
 
     for msg in messages:
-        msg_type = msg['type']
-        for field in MSG_TYPE_FIELDS[msg_type]:
-            if not MSG_VALIDATION_LOOKUP[field]['test'](msg[field]):
-                if err is not None:
-                    err.write("Field `%s' failed: %s" % (field, json.dumps(msg) + os.linesep))
-                return_val = False
+
+        # Make sure the message specifies its type and that the type is one we can validate
+        if 'type' not in msg or msg['type'] not in MSG_TYPE_FIELDS:
+            if err is not None:
+                err.write("No 'type' key in msg or type is invalid or not testable: %s" % msg)
+            return_val = False
+
+        # Normal field validation
+        else:
+            msg_type = msg['type']
+            for field in MSG_TYPE_FIELDS[msg_type]:
+                if not MSG_VALIDATION_LOOKUP[field]['test'](msg[field]):
+                    if err is not None:
+                        sys.stdout.write("Field `%s' failed: %s" % (field, json.dumps(msg) + os.linesep))
+                    return_val = False
 
     return return_val
 
@@ -341,20 +375,32 @@ def validate_messages(messages, err=None):
 class TestValidateMessages(unittest.TestCase):
 
     def test_all_types(self):
-        for msg_type, fields in MSG_TYPE_FIELDS.items():
-            good_message = {f: MSG_VALIDATION_LOOKUP[f]['good'] for f in fields}
+        for msg_type, msg_fields in MSG_TYPE_FIELDS.items():
+
+            # Check type field individually since the other tests force it to be correct
+            assert not validate_messages([{'field': 'val'}])
+            assert not validate_messages([{'type': MSG_VALIDATION_LOOKUP['type']['bad']}])
+
+            # Construct a good message
+            good_message = {f: MSG_VALIDATION_LOOKUP[f]['good'] for f in msg_fields}
             good_message['type'] = msg_type
-            bad_message = {f: MSG_VALIDATION_LOOKUP[f]['bad'] for f in fields}
-            bad_message['type'] = msg_type
-            from StringIO import StringIO
-            e = StringIO()
-            print(validate_messages([good_message], err=e))
-            e.seek(0)
-            print(e.read())
-            return
-            assert validate_messages([good_message])
-            assert not validate_messages([bad_message])
+
+            assert validate_messages([good_message]), \
+                "Supposed 'good' msg failed validation: %s" % good_message
+
+            # Creating a bad message from all of the bad values is an insufficient test because the validator
+            # will start checking fields and as soon as it gets to a bad one it will flag the message as invalid.
+            # Every field is checked in every message and every bad field is logged but we can't validate individual
+            # fields without taking a good message and then changing one field at a time to a bad field.
+            for field in msg_fields:
+                if field != 'type':
+                    bad_message = good_message.copy()
+                    bad_message[field] = MSG_VALIDATION_LOOKUP[field]['bad']
+                    assert not validate_messages([bad_message]), \
+                        "Field `%s' should have caused message to fail: %s" % (field, bad_message)
 
 
 if __name__ == '__main__':
-    sys.exit(unittest.main())
+    if len(sys.argv) > 1 and sys.argv[1].lower() == 'test':
+        del sys.argv[1]
+        sys.exit(unittest.main())
