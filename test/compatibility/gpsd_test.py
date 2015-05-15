53c104bbffba173e580c232f16a28aa6e63a70da
Egil Moeller <egil.moller@freecode.no>
2015-05-15 11:17:43 +0200

    Moved compatibility tests to follow the common structure

--- test/compatibility/gpsd_test.py
+++ test/compatibility/gpsd_test.py
@@ -2,16 +2,35 @@
 
 """Tests for ais.compatibility.gpsd."""
 
-import unittest
-
 import ais
-from ais.compatibility import gpsd
+import ais.compatibility.gpsd
+import ais.stream
+
+import unittest
+import json
+import os
+import re
+import subprocess
+import six
+from .. import testutils
+
+known_problems = {
+    2: set(('turn', 'status_text')),
+    9: set(['speed']),
+    15: set(['mmsi2']),
+    17: set(('lat', 'lon')),
+    20: set((
+        'increment3', 'number3', 'offset3', 'timeout3',
+        'increment4', 'number4', 'offset4', 'timeout4',
+    )),
+    27: set(['status']),
+}
 
 
-class GpsdTest(unittest.TestCase):
+class SingleMessageTestsTest(unittest.TestCase):
 
   def setUp(self):
-    self.mangle = gpsd.Mangler()
+    self.mangle = ais.compatibility.gpsd.Mangler()
 
   def testMsg1(self):
     fields = '!AIVDM,1,1,,B,169A91O005KbT4gUoUl9d;5j0D0U,0*2D'.split(',')
@@ -51,6 +70,128 @@ class GpsdTest(unittest.TestCase):
         'shiptype_text': '204 - Unknown',
         'type': 5}
     self.assertDictContainsSubset(expected, mangled)
+
+
+class StreamingTest(unittest.TestCase):
+  def setUp(self):
+    self.dir = os.path.split(__file__)[0]
+
+  def validate_file(self, base):
+    nmea_name = os.path.join(self.dir, base + '.nmea')
+    json_name = os.path.join(self.dir, base + '.json')
+
+    def Json():
+      with open(json_name) as f:
+        for msg in f:
+          yield json.loads(msg)
+
+    def Libais():
+      with open(nmea_name) as f:
+        for msg in ais.stream.decode(f):
+          yield ais.compatibility.gpsd.mangle(msg)
+
+    g = iter(Json())
+    a = iter(Libais())
+
+    try:
+      while True:
+        gmsg = six.advance_iterator(g)
+        amsg = six.advance_iterator(a)
+        while amsg['type'] != gmsg['type']:
+          amsg = six.advance_iterator(a)
+
+        if gmsg['type'] in known_problems:
+          for key in known_problems[gmsg['type']]:
+            if key in gmsg: del gmsg[key]
+            if key in amsg: del amsg[key]
+
+        diff = testutils.DictDiff(gmsg, amsg)
+        self.assertFalse(diff['changed'])
+        self.assertFalse(
+            diff['removed'],
+            'Removed not empty: %s\n  %s\n  %s' % (
+                diff['removed'],
+                amsg,
+                gmsg))
+
+    except StopIteration:
+      pass
+
+  def testTypeExamples(self):
+      self.validate_file("../data/typeexamples")
+
+  def testTagblock(self):
+      self.validate_file("../data/tagblock")
+
+
+
+def HaveGpsdecode():
+  """Return true if the gpsdecode binary is on the path or false if not."""
+  try:
+    subprocess.check_call(['gpsdecode', '-V'])
+    return True
+  except OSError:
+    return False
+
+
+class TestActualGPSDCompatibility(unittest.TestCase):
+  def setUp(self):
+    self.dir = os.path.split(__file__)[0]
+
+  def validate_file(self, base):
+    nmea_name = os.path.join(self.dir, base + '.nmea')
+    json_name = os.path.join(self.dir, base + '.gpsdecode.json')
+
+    subprocess.check_call('gpsdecode < %s > %s' % (nmea_name, json_name),
+                          shell=True)
+
+    try:
+      def Gpsd():
+        with open(json_name) as f:
+          for msg in f:
+            yield json.loads(msg)
+
+      def Libais():
+        with open(nmea_name) as f:
+          for msg in ais.stream.decode(f):
+            yield ais.compatibility.gpsd.mangle(msg)
+
+      g = iter(Gpsd())
+      a = iter(Libais())
+
+      try:
+        while True:
+          gmsg = six.advance_iterator(g)
+          amsg = six.advance_iterator(a)
+          while amsg['type'] != gmsg['type']:
+            amsg = six.advance_iterator(a)
+
+          if gmsg['type'] in known_problems:
+            for key in known_problems[gmsg['type']]:
+              if key in gmsg: del gmsg[key]
+              if key in amsg: del amsg[key]
+
+          diff = testutils.DictDiff(gmsg, amsg)
+          self.assertFalse(diff['changed'])
+          self.assertFalse(
+              diff['removed'],
+              'Removed not empty: %s\n  %s\n  %s' % (
+                  diff['removed'],
+                  amsg,
+                  gmsg))
+
+      except StopIteration:
+        pass
+    finally:
+      os.unlink(json_name)
+
+  @unittest.skipIf(not HaveGpsdecode(), 'gpsdecode not in the path')
+  def testTypeExamples(self):
+      self.validate_file("../data/typeexamples")
+
+  @unittest.skipIf(not HaveGpsdecode(), 'gpsdecode not in the path')
+  def testTagblock(self):
+      self.validate_file("../data/tagblock")
 
 
 if __name__ == '__main__':
