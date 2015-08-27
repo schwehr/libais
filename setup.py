2c69ee619b94bfc8c1a6bd5c2bb720a3ee1fb91b
Kurt Schwehr <schwehr@google.com>
2015-08-27 12:34:41 -0700

    Add missing comma between entry_point lines.
    The 2nd line in console_scripts found that I was not putting a comma a
    the end of each line.  Strings were the concatinated giving a single bogus entry.
    
    Closes schwehr/libais#105

--- setup.py
+++ setup.py
@@ -89,8 +89,8 @@ setup(name='libais',
       scripts=["bin/aisdecode"],
       entry_points={
           'console_scripts': [
-              'libais_decode = ais.decode:main'
-              'libais_stats = ais.stats:main'
+              'libais_decode = ais.decode:main',
+              'libais_stats = ais.stats:main',
           ]
       },
       test_suite = "test"
