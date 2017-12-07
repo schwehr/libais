25b3e519bafa0ad1f10d81f37c81ea993a24abab
Kurt Schwehr <schwehr@google.com>
2017-12-06 16:01:28 -0800

    Add tests of 6:1:5 messages in issue 137
    Closes #137

--- src/test/ais6_test.cc
+++ src/test/ais6_test.cc
@@ -256,6 +256,22 @@ TEST(Ais6_1_5, Issue162) {
   ValidateAis6_1_5(*msg, 7, false, 0, 0, 0);
 }
 
+TEST(Ais6_1_5, Issue137_Part1) {
+  // !AIVDM,1,1,,B,65E4;r5I0Q9R04D0@01000000000,0*69
+  std::unique_ptr<Ais6_1_5> msg(
+      new Ais6_1_5("677IKl=HQGw004D0@02000000000", 0));
+  ValidateAis6(msg.get(), 0, 477518800, 3, 371286000, true, 0, 1, 5);
+  ValidateAis6_1_5(*msg, 4, false, 0, 0, 0);
+}
+
+TEST(Ais6_1_5, Issue137_Part2) {
+  // !AIVDM,1,1,,A,677Dg2<s=9o004D0@16P00000000,0*72
+  std::unique_ptr<Ais6_1_5> msg(
+      new Ais6_1_5("677IKl=HQGw004D0@02000000000", 0));
+  ValidateAis6(msg.get(), 0, 477518800, 3, 371286000, true, 0, 1, 5);
+  ValidateAis6_1_5(*msg, 4, false, 0, 0, 0);
+}
+
 // TODO(schwehr): Test Ais6_1_12.
 // TODO(schwehr): Test Ais6_1_14.
 // TODO(schwehr): Test Ais6_1_18.
