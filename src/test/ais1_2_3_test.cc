b4f1b22afbc238c7ff88d9c09bb58c614840a759
Kurt Schwehr <schwehr@google.com>
2025-08-24 22:21:17 -0700

    ais1_2_3_test.cc: Fix modernize-use-bool-literals and misc-const-correctness

--- src/test/ais1_2_3_test.cc
+++ src/test/ais1_2_3_test.cc
@@ -128,11 +128,11 @@ void ValidateCommState(
 
 TEST(Ais123Test, Decode1ReceivedStations) {
   // Verifies decode of Msg 1 SOTDMA with received_stations.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,A,100WhdhP0nJRdiFFHFvm??v00L12,0*13,raishub,1342569600");
   Validate(
       msg.get(), 1, 0, 651443, 0, -128, -731.386474609375, true, 5.4, 0,
-      -76.30584833333333, 39.112951666666667, 511, 0, 0, 0, false);
+      -76.30584833333333, 39.112951666666667, 511, 0, false, 0, false);
 
   ValidateCommState(
       msg.get(), 0, true, 7, true, 66, false, 0, false, 0, 0, 0, false, 0,
@@ -141,12 +141,12 @@ TEST(Ais123Test, Decode1ReceivedStations) {
 
 TEST(Ais123Test, Decode1Utc) {
   // Verifies decode of Msg with UTC comm state.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,B,181:Kjh01ewHFRPDK1s3IRcn06sd,0*08,raishub,1342569600");
 
   Validate(
       msg.get(), 1, 0, 538090443, 0, 0, 0.0, false, 10.8999996185, 1,
-      -8.6611999999999991, 35.690633333333331, 85, 59, 0, 0, false);
+      -8.6611999999999991, 35.690633333333331, 85, 59, false, 0, false);
 
   ValidateCommState(
       msg.get(), 0, true, 1, false, 0, false, 0, true, 23, 59, 0, false, 0,
@@ -155,12 +155,12 @@ TEST(Ais123Test, Decode1Utc) {
 
 TEST(Ais123Test, Decode1SlotNumber) {
   // Verifies decode of Msg with slot number.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,B,16<qIn0P018MNT6=v:KMH?wf0@Ph,0*29,raishub,1342569600");
 
   Validate(
       msg.get(), 1, 0, 416176600, 0, -128, -731.386474609, true, 0.10000000149,
-      0, 118.28757833333333, 24.416608333333333, 511, 55, 0, 0, false);
+      0, 118.28757833333333, 24.416608333333333, 511, 55, false, 0, false);
 
   ValidateCommState(
       msg.get(), 0, true, 4, false, 0, true, 2096, false, 0, 0, 0, false, 0,
@@ -169,11 +169,11 @@ TEST(Ais123Test, Decode1SlotNumber) {
 
 TEST(Ais123Test, Decode1NoGPS) {
   // Verifies decode of Msg without a GPS lock.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,B,13aIjG?P?w<tSF0l4Q@>401v1`FO,0*04,raishub,1342569600");
   Validate(
       msg.get(), 1, 0, 244740700, 15, -128, -731.386474609, true, 102.3, 0,
-      181.0, 91.0, 0, 63, 0, 0, false);
+      181.0, 91.0, 0, 63, false, 0, false);
 
   ValidateCommState(
       msg.get(), 3, true, 2, false, 0, true, 1439, false, 0, 0, 0, false, 0,
@@ -182,12 +182,12 @@ TEST(Ais123Test, Decode1NoGPS) {
 
 TEST(Ais123Test, Decode2Rot0) {
   // Verifies decode of Msg 2 driving straight ahead.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,B,2341N:0000PCTfPMHAQoP8442<;0,0*33,raishub,1342569601");
 
   Validate(
       msg.get(), 2, 0, 205545000, 0, 0, 0.0, false, 0.0, 1, 4.275973333333333,
-      51.344011666666667, 258, 2, 0, 0, true);
+      51.344011666666667, 258, 2, false, 0, true);
 
   ValidateCommState(
       msg.get(), 0, true, 3, true, 704, false, 0, false, 0, 0, 0, false, 0,
@@ -196,12 +196,12 @@ TEST(Ais123Test, Decode2Rot0) {
 
 TEST(Ais123Test, Decode2Turning) {
   // Verifies decode of Msg 2 for a ship in a turn.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,B,284;UGTdP4301>3L;B@Wk3TnU@A1,0*7C,raishub,1342572913");
 
   Validate(
       msg.get(), 2, 0, 541255006, 4, -78, -271.591522217, false, 0.40000000596,
-      0, 41.947201666666665, -62.606396666666669, 114, 27, 1, 1, false);
+      0, 41.947201666666665, -62.606396666666669, 114, 27, true, 1, false);
 
   ValidateCommState(
       msg.get(), 2, true, 4, false, 0, true, 1089, false, 0, 0, 0, false, 0,
@@ -210,12 +210,12 @@ TEST(Ais123Test, Decode2Turning) {
 
 TEST(Ais123Test, Decode3) {
   // Verifies decode of Msg 3.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,B,34hoV<5000Jw95`GWokbFTuf0000,0*6C,raishub,1342569600");
 
   Validate(
       msg.get(), 3, 0, 319678000, 5, 0, 0.0, false, 0.0, 0, -70.0925,
-      41.284183333333331, 158, 55, 0, 0, false);
+      41.284183333333331, 158, 55, false, 0, false);
 
   ValidateCommState(
       msg.get(), 0, false, 0, false, 0, false, 0, false, 0, 0, 0, false, 0,
@@ -225,12 +225,12 @@ TEST(Ais123Test, Decode3) {
 
 TEST(Ais123Test, Decode3KeepTrue) {
   // Verifies decode of Msg 3 with non-zero slot and keep flag true.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,B,35PH9M0P1VJe0GHG@NCeqh5n006S,0*10,raishub,1342569600");
 
   Validate(
       msg.get(), 3, 0, 369494388, 0, -128, -731.386474609, true, 10.1999998093,
-      0, -74.054433333333336, 40.64525, 2, 59, 0, 0, false);;
+      0, -74.054433333333336, 40.64525, 2, 59, false, 0, false);
 
   ValidateCommState(
       msg.get(), 0, false, 0, false, 0, false, 0, false, 0, 0, 0, false, 0,
@@ -239,12 +239,12 @@ TEST(Ais123Test, Decode3KeepTrue) {
 
 TEST(Ais123Test, Decode3Special) {
   // Verifies decode of Msg 3 with a really large slot increment.
-  std::unique_ptr<Ais1_2_3> msg = Init(
+  const std::unique_ptr<Ais1_2_3> msg = Init(
       "!AIVDM,1,1,,A,33aI;sPP00PD<sPMd8<P0?v0RC?C,0*14,raishub,1342569600");
 
   Validate(
       msg.get(), 3, 0, 244730862, 0, -128, -731.386474609, true, 0.0, 1,
-      4.4132, 51.886163333333336, 511, 0, 1, 0, true);
+      4.4132, 51.886163333333336, 511, 0, true, 0, true);
 
   ValidateCommState(
       msg.get(), 0, false, 0, false, 0, false, 0, false, 0, 0, 0, false, 0,
